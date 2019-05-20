c8202cbd5e3f64621ea27ea97c40209a19ff8b18
JS Academy <vflores.daniela@gmail.com>
2019-05-20 08:22:08 -0700

    EjerciciosJS

--- workHere.js
+++ workHere.js
@@ -1,10 +1,16 @@
-var billionaires = require('./billionaires.json');
- 
+var billionaires = require('./billionaires.json'); 
 /*
     Get the names of the billionaires that are in their 20s
 */
 
-let twentyYearOldBillionaires = null;
+let twentyYearOldBillionaires = (arr)=>{
+  arr.forEach(x=>{
+    if(x.demographics.age>=20 && x.demographics.age<30) {
+        console.log(x.name);
+    }
+    })
+}
+twentyYearOldBillionaires(billionaires);
 
 /*
     create a new object with the billionairse year, name and worth rounded to the newarest integer
@@ -15,32 +21,56 @@ let twentyYearOldBillionaires = null;
     }
 */
 
-let roundedWorth = null;
+let roundedWorth = billionaires.map (x=>{
+    console.log ({
+        year: x.year,
+        Name: x.name,
+        wealth: Math.round(x.wealth["worth in billions"])
+    })
+});
+
 
 /*
     sum the rounded worth of all the billionaires
 */
 
-let aggregatedWorth = null;
-// console.log(aggregatedWorth)
+let aggregatedWorth= billionaires.reduce((prev,curr)=>{
+        return prev+Math.round(curr.wealth["worth in billions"]);
+    },0);
+console.log(aggregatedWorth)
 
 /*
     find the the richest billionaire, make sure the result is in an array
 */
 
-let richest = null
-
+let richest = (arr)=>{
+     arr.forEach(x=>{
+        if(x.rank===1) {
+        console.log(x.name);
+        }
+    })
+}
+richest(billionaires);
+      
 /*
     find the poorest billioanire, make sure the result is in an array
 */
 
-let poorest = null;
+//NO JALA
+let poorest = [billionaires.reduce((prev,element)=>{
+    if(Object.keys(prev).length=== Object.keys({}).length){
+        prev=element;
+        return prev;
+    }
+    return prev;
+    })
+)];
 
-/*
-    make a new array containing the richest and the poorest billionaire
+
+/* make a new array containing the richest and the poorest billionaire
 */
 
-let richAndPoor = null;
+//let richAndPoor = null;
 
 module.exports = {
     twentyYearOldBillionaires,
@@ -48,5 +78,6 @@ module.exports = {
     aggregatedWorth,
     richest,
     poorest,
-    richAndPoor
-}
+    /* richAndPoor
+*/
+};
